b64494fb599670efa46f43f967457dfd966a0e20
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-05-13 13:53:26 +0530

    bottom navigation

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,12 +1,108 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React from "react";
 
 const Home = () => {
   return (
-    <View>
-      <Text>Home</Text>
-    </View>
-  )
-}
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          width: "100%",
+          height: 70,
+          backgroundColor: "#fff",
+          position: "absolute",
+          bottom: 0,
+          flexDirection: "row",
+          alignItems: "center",
+        }}
+      >
+        {/* home */}
+        <TouchableOpacity
+          style={{
+            width: "20%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("../Images/home.png")}
+            style={{ width: 34, height: 34 }}
+          />
+        </TouchableOpacity>
 
-export default Home
+        {/* search */}
+        <TouchableOpacity
+          style={{
+            width: "20%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("../Images/search.png")}
+            style={{ width: 34, height: 34 }}
+          />
+        </TouchableOpacity>
+
+        {/* bag */}
+        <View
+          style={{
+            width: "20%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <TouchableOpacity
+            style={{
+              width: 45,
+              height: 45,
+              backgroundColor: "#000",
+              borderRadius: 25,
+              justifyContent: "center",
+              alignItems: "center",
+            }}
+          >
+            <Image
+              source={require("../Images/bag.png")}
+              style={{ width: 34, height: 34, tintColor: "#fff" }}
+            />
+          </TouchableOpacity>
+        </View>
+
+        {/* heart */}
+        <TouchableOpacity
+          style={{
+            width: "20%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("../Images/heart.png")}
+            style={{ width: 34, height: 34 }}
+          />
+        </TouchableOpacity>
+
+        {/* user */}
+        <TouchableOpacity
+          style={{
+            width: "20%",
+            height: "100%",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("../Images/user.png")}
+            style={{ width: 34, height: 34 }}
+          />
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
+};
+
+export default Home;
