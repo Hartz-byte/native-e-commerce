2cd4e1caae6d0583ef1277d7a64e5bbd97df9d24
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-05-13 14:27:35 +0530

    bottom nav bar

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,9 +1,30 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
+
+import Main from "../BottomNavScreens/Main";
+import Search from "../BottomNavScreens/Search";
+import Cart from "../BottomNavScreens/Cart";
+import Wishlist from "../BottomNavScreens/Wishlist";
+import Profile from "../BottomNavScreens/Profile";
 
 const Home = () => {
+  const [selectedTab, setSelectedTab] = useState(0);
+
   return (
     <View style={{ flex: 1 }}>
+      {/* tabs condition */}
+      {selectedTab == 0 ? (
+        <Main />
+      ) : selectedTab == 1 ? (
+        <Search />
+      ) : selectedTab == 2 ? (
+        <Cart />
+      ) : selectedTab == 3 ? (
+        <Wishlist />
+      ) : (
+        <Profile />
+      )}
+
       <View
         style={{
           width: "100%",
@@ -23,10 +44,17 @@ const Home = () => {
             justifyContent: "center",
             alignItems: "center",
           }}
+          onPress={() => {
+            setSelectedTab(0);
+          }}
         >
           <Image
             source={require("../Images/home.png")}
-            style={{ width: 34, height: 34 }}
+            style={{
+              width: 34,
+              height: 34,
+              tintColor: selectedTab === 0 ? "#000" : "#8e8e8e",
+            }}
           />
         </TouchableOpacity>
 
@@ -38,10 +66,17 @@ const Home = () => {
             justifyContent: "center",
             alignItems: "center",
           }}
+          onPress={() => {
+            setSelectedTab(1);
+          }}
         >
           <Image
             source={require("../Images/search.png")}
-            style={{ width: 34, height: 34 }}
+            style={{
+              width: 34,
+              height: 34,
+              tintColor: selectedTab === 1 ? "#000" : "#8e8e8e",
+            }}
           />
         </TouchableOpacity>
 
@@ -58,15 +93,22 @@ const Home = () => {
             style={{
               width: 45,
               height: 45,
-              backgroundColor: "#000",
+              backgroundColor: selectedTab === 2 ? "green" : "#000",
               borderRadius: 25,
               justifyContent: "center",
               alignItems: "center",
             }}
+            onPress={() => {
+              setSelectedTab(2);
+            }}
           >
             <Image
               source={require("../Images/bag.png")}
-              style={{ width: 34, height: 34, tintColor: "#fff" }}
+              style={{
+                width: 34,
+                height: 34,
+                tintColor: "#fff",
+              }}
             />
           </TouchableOpacity>
         </View>
@@ -79,10 +121,17 @@ const Home = () => {
             justifyContent: "center",
             alignItems: "center",
           }}
+          onPress={() => {
+            setSelectedTab(3);
+          }}
         >
           <Image
             source={require("../Images/heart.png")}
-            style={{ width: 34, height: 34 }}
+            style={{
+              width: 34,
+              height: 34,
+              tintColor: selectedTab === 3 ? "#000" : "#8e8e8e",
+            }}
           />
         </TouchableOpacity>
 
@@ -94,10 +143,17 @@ const Home = () => {
             justifyContent: "center",
             alignItems: "center",
           }}
+          onPress={() => {
+            setSelectedTab(4);
+          }}
         >
           <Image
             source={require("../Images/user.png")}
-            style={{ width: 34, height: 34 }}
+            style={{
+              width: 34,
+              height: 34,
+              tintColor: selectedTab === 4 ? "#000" : "#8e8e8e",
+            }}
           />
         </TouchableOpacity>
       </View>
