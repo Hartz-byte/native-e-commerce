38200094bc7893b964a8d3b5fd76743a0b1002fd
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-05-14 14:08:20 +0530

    update

--- src/BottomNavScreens/Main.js
+++ src/BottomNavScreens/Main.js
@@ -1,11 +1,24 @@
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import React from "react";
 import Header from "../Common/Header";
 
 const Main = () => {
   return (
     <View style={{ flex: 1 }}>
+      {/* header */}
       <Header />
+
+      {/* banner */}
+      <Image
+        source={require("../Images/banner.webp")}
+        style={{
+          width: "94%",
+          height: 200,
+          borderRadius: 10,
+          alignSelf: "center",
+          marginTop: 20,
+        }}
+      />
     </View>
   );
 };
---
+++ src/Products.js
@@ -0,0 +1,214 @@
+export const products = {
+  category: [
+    {
+      category: "T Shirt",
+      data: [
+        {
+          name: "t-shirt 1",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "t-shirt 2",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "t-shirt 3",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "t-shirt 4",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "t-shirt 5",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+      ],
+    },
+    {
+      category: "Jeans",
+      data: [
+        {
+          name: "Jeans 1",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Jeans 2",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Jeans 3",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Jeans 4",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Jeans 5",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+      ],
+    },
+    {
+      category: "Shoes",
+      data: [
+        {
+          name: "Shoes 1",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Shoes 2",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Shoes 3",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Shoes 4",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Shoes 5",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+      ],
+    },
+    {
+      category: "Jacket",
+      data: [
+        {
+          name: "Jacket 1",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Jacket 2",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Jacket 3",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Jacket 4",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Jacket 5",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+      ],
+    },
+    {
+      category: "Slipper",
+      data: [
+        {
+          name: "Slipper 1",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Slipper 2",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Slipper 3",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Slipper 4",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Slipper 5",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+      ],
+    },
+    {
+      category: "Trousers",
+      data: [
+        {
+          name: "Trouser 1",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Trouser 2",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Trouser 3",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Trouser 4",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+        {
+          name: "Trouser 5",
+          price: 300,
+          gender: "male",
+          image: require("./Images/banner.webp"),
+        },
+      ],
+    },
+  ],
+};
