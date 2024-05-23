604886f2d69bb754f6a71788dce36ced4cd15cc5
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-05-23 16:02:12 +0530

    cart and fav done

--- src/BottomNavScreens/Cart.js
+++ src/BottomNavScreens/Cart.js
@@ -1,12 +1,35 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, FlatList } from "react-native";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import CartItem from "../Common/CartItem";
+import { addToWishlist, removeFromCart } from "../Redux/actions/Actions";
 
 const Cart = () => {
-  return (
-    <View>
-      <Text>Cart</Text>
-    </View>
-  )
-}
+  const [cartList, setCartList] = useState([]);
+  const cartData = useSelector((state) => state.reducers);
 
-export default Cart
+  const dispatch = useDispatch();
+
+  return (
+    <View style={{ flex: 1, marginTop: 50, marginBottom: 80 }}>
+      <FlatList
+        data={cartData}
+        renderItem={({ item, index }) => {
+          return (
+            <CartItem
+              item={item}
+              onRemoveItem={() => {
+                dispatch(removeFromCart(index));
+              }}
+              onAddToWishlist={(x) => {
+                dispatch(addToWishlist(x));
+              }}
+            />
+          );
+        }}
+      />
+    </View>
+  );
+};
+
+export default Cart;
