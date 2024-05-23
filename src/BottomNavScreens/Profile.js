383c1b7f6e79c321f23b26aca5c6a7cc97e1a890
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-05-23 16:43:23 +0530

    profile update

--- src/BottomNavScreens/Profile.js
+++ src/BottomNavScreens/Profile.js
@@ -1,12 +1,65 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TouchableOpacity, Image } from "react-native";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 const Profile = () => {
-  return (
-    <View>
-      <Text>Profile</Text>
-    </View>
-  )
-}
+  const [name, setName] = useState("");
 
-export default Profile
+  useEffect(() => {
+    getData();
+  });
+
+  const getData = async () => {
+    retrievedName = await AsyncStorage.getItem("NAME");
+    setName(retrievedName);
+  };
+
+  return (
+    <View style={{ flex: 1 }}>
+      <View
+        style={{
+          width: "100%",
+          height: 70,
+          justifyContent: "space-between",
+          alignItems: "center",
+          flexDirection: "row",
+          marginTop: 30,
+        }}
+      >
+        {/* profile text */}
+        <Text style={{ fontWeight: "600", fontSize: 18, marginLeft: 15 }}>
+          Profile
+        </Text>
+
+        {/* settings button */}
+        <TouchableOpacity
+          style={{
+            width: 30,
+            height: 30,
+            marginRight: 20,
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            source={require("../Images/setting.png")}
+            style={{ width: 24, height: 24 }}
+          />
+        </TouchableOpacity>
+      </View>
+
+      {/* profile icon */}
+      <Image
+        source={require("../Images/profile.png")}
+        style={{ width: 80, height: 80, alignSelf: "center", marginTop: 30 }}
+      />
+
+      {/* user's name */}
+      <Text style={{ alignSelf: "center", marginTop: 20, fontSize: 18 }}>
+        {name}
+      </Text>
+    </View>
+  );
+};
+
+export default Profile;
