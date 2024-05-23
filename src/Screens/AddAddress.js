26805b69036850e6f4b1f30ed8678db70553f806
Hartz-byte <67004713+Hartz-byte@users.noreply.github.com>
2024-05-23 21:52:12 +0530

    address update

--- src/Screens/AddAddress.js
+++ src/Screens/AddAddress.js
@@ -1,9 +1,15 @@
 import { View, Text, TouchableOpacity, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { useNavigation } from "@react-navigation/native";
+import CustomTextInput from "../Common/CustomTextInput";
+import CommonButton from "../Common/CommonButton";
 
 const AddAddress = () => {
   const navigation = useNavigation();
+
+  const [city, setCity] = useState("");
+  const [building, setBuilding] = useState("");
+  const [pin, setPin] = useState("");
 
   return (
     <View style={{ flex: 1 }}>
@@ -36,6 +42,39 @@ const AddAddress = () => {
           />
         </TouchableOpacity>
       </View>
+
+      {/* city name */}
+      <CustomTextInput
+        placeholder={"Enter City Name"}
+        value={city}
+        onChangeText={(txt) => setCity(txt)}
+        icon={require("../Images/city.png")}
+      />
+
+      {/* building name */}
+      <CustomTextInput
+        placeholder={"Enter Building Name"}
+        value={building}
+        onChangeText={(txt) => setBuilding(txt)}
+        icon={require("../Images/building.png")}
+      />
+
+      {/* pincode */}
+      <CustomTextInput
+        placeholder={"Enter Pincode"}
+        value={pin}
+        keyboardType={"number-pad"}
+        onChangeText={(txt) => setPin(txt)}
+        icon={require("../Images/pincode.png")}
+      />
+
+      {/* save button */}
+      <CommonButton
+        title={"Save Address"}
+        bgColor={"#000"}
+        textColor={"#fff"}
+        onPress={() => {}}
+      />
     </View>
   );
 };
